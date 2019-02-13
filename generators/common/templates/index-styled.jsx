fa6a0cad3757b4a60449824ea97e0e0710829b18
Anton Babakhin <anton-098+ya@yandex.ru>
2019-02-14 02:13:07 +0300

    Fix css from styled-components issue

--- generators/common/templates/index-styled.jsx
+++ generators/common/templates/index-styled.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Button = styled.a`  
   display: inline-block;
